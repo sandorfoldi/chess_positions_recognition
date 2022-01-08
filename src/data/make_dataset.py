1917137a1637b2feb48b5f26b1f54f51c3ea5ade
Hans Christian Milman <hanschrmil@gmail.com>
2022-01-08 17:28:05 +0100

    make_data faster
    used numba to make it a bit faster. Also updated reqs

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,38 +3,62 @@ from typing import List
 
 import numpy as np
 from PIL import Image
+from tqdm import tqdm
+from numba import njit
 
+
+@njit
 def crop(image: np.ndarray) -> List[np.ndarray]:
     parts = []
     for r in range(0, image.shape[0], 50):
         for c in range(0, image.shape[1], 50):
-            parts.append(image[r: r + 50, c:c + 50, :])
+            parts.append(image[r : r + 50, c : c + 50, :])
     return parts
 
 
+@njit
 def transform_label(filename: str) -> List[str]:
     orig_label = filename.split(".")[0]
     transformed_label = []
     ranks = orig_label.split("-")
     for rank in ranks:
         for letter in rank:
-            if letter in list("0123456789"):
-                transformed_label.extend(list(int(letter) * "E"))
+            if letter in "0123456789":
+                transformed_label.extend(
+                    (ord(letter) - 48) * "E"
+                )  # use ord cuz numba don't like castings
             else:
                 transformed_label.append(letter)
     return transformed_label
 
 
-def make_dataset(data_size: int = 100,
-    input_dir: str = "data/raw/train", output_dir: str = "data/processed/train") -> None:
-    files = os.listdir(input_dir)[:data_size]
-    for idx, file in enumerate(files):
+def make_dataset(
+    input_dir: str = "data/raw/train", output_dir: str = "data/processed/train"
+) -> None:
+
+    dirs = [
+        "b_b",
+        "b_k",
+        "b_n",
+        "b_p",
+        "b_q",
+        "b_r",
+        "w_B",
+        "w_K",
+        "w_N",
+        "w_P",
+        "w_Q",
+        "w_R",
+        "w_E",
+    ]
+    for item in dirs:
+        os.makedirs(f"{output_dir}/{item}", exist_ok=True)
+
+    files = os.listdir(input_dir)[:20000]
+
+    # for idx, file in enumerate(tqdm(files)):
+    for idx, file in enumerate(tqdm(files)):
         folder_names = transform_label(file)
-        for name in folder_names:
-            if not name.isupper():
-                os.makedirs(f"{output_dir}/b_{name}", exist_ok=True)
-            else:
-                os.makedirs(f"{output_dir}/w_{name}", exist_ok=True)
         orig_image = Image.open(f"{input_dir}/{file}")
         orig_image = np.array(orig_image)
         cropped_images = crop(orig_image)
@@ -44,6 +68,7 @@ def make_dataset(data_size: int = 100,
                 image.save(f"{output_dir}/b_{folder_names[i]}/{idx}-{i}.jpeg")
             else:
                 image.save(f"{output_dir}/w_{folder_names[i]}/{idx}-{i}.jpeg")
+    os.rename(f"{output_dir}/w_E", f"{output_dir}/E")
 
 
 if __name__ == "__main__":
