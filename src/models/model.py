41468ba03e28c0a9d3782e29f039e96121d16040
Rasmus Spangsberg <rasmus.spangsberg@hotmail.com>
2022-01-11 00:41:45 +0100

    Added support for GPU. We now split the training data into test and train sets and print accuracies on both. We can now continue to train from a previously trained model.

--- src/models/model.py
+++ src/models/model.py
@@ -1,18 +1,15 @@
 import kornia.contrib as K
-import torch.nn as nn
 import torch
+import torch.nn as nn
 
 
 class ChessPiecePredictor(nn.Module):
     def __init__(self):
         super().__init__()
-        self.inp = K.VisionTransformer(image_size=50, patch_size=50)
+        self.inp = K.VisionTransformer(image_size=50, patch_size=5, in_channels=1)
         self.out = K.ClassificationHead(num_classes=13)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # make sure input tensor is flattened
-        #x = x.view(x.shape[0], -1)
-
         x = self.inp(x)
         x = self.out(x)
 
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,61 +1,162 @@
 import argparse
+import math
 import sys
+from pathlib import Path
+from time import time
 
 import matplotlib.pyplot as plt
 import torch
 import torchvision
-from torchvision import transforms
 from model import ChessPiecePredictor
-from torch import nn, optim
+from torch import nn
 from torch.utils.data import DataLoader
+from torchvision import transforms
 
 
 def train():
-    print("Training started...")
-    parser = argparse.ArgumentParser(description='Training arguments')
-    parser.add_argument('load_data_from', default="")
+    parser = argparse.ArgumentParser(description="Training arguments")
+    parser.add_argument("load_data_from", default="")
+    # used for loading and training a model
+    parser.add_argument("--continue_training_from", required=False, type=Path)
     args = parser.parse_args(sys.argv[1:])
 
-    t = transforms.Compose([
-        transforms.ToTensor(),
-    ])
-    train_data = torchvision.datasets.ImageFolder(args.load_data_from, transform=t)
-    train_loader = DataLoader(train_data, batch_size=256,
-                              shuffle=True, num_workers=4)
+    start_time = time()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
+
+    print("Loading data...")
+    t = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Grayscale(),
+        ]
+    )
+
+    data_set = torchvision.datasets.ImageFolder(args.load_data_from, transform=t)
+    train_size = math.ceil(len(data_set) * 0.85)
+    validation_size = math.floor(len(data_set) * 0.15)
+
+    train_data, validation_data = torch.utils.data.random_split(
+        data_set, [train_size, validation_size]
+    )
+
+    batch_size = 4
+    train_loader = DataLoader(
+        train_data, batch_size=batch_size, pin_memory=True, num_workers=4, shuffle=True
+    )
+    validation_loader = DataLoader(
+        validation_data,
+        batch_size=batch_size,
+        pin_memory=True,
+        num_workers=4,
+        shuffle=False,
+    )
 
     model = ChessPiecePredictor()
-    criterion = nn.NLLLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.003)
 
-    losses = []
+    if args.continue_training_from:
+        print(f"Loading model from {args.continue_training_from} ...")
+        checkpoint = torch.load(args.continue_training_from)
+        model.load_state_dict(checkpoint)
 
-    epochs = 1
-    i = 0
+    model.to(device)
+    criterion = nn.CrossEntropyLoss()
+
+    learning_rate = 1e-4
+    momentum = 0.9
+    weight_decay = 0.0000001
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=learning_rate,
+        momentum=momentum,
+        weight_decay=weight_decay,
+    )
+
+    print("Training started...")
+    train_losses = []
+    validation_losses = []
+
+    batch_count = int(train_size / batch_size)
+    epochs = 2
     for e in range(epochs):
-        print("epoch:", e)
-        running_loss = 0
+        train_loss = 0
+        train_correct = 0
+
+        validation_loss = 0
+        validation_correct = 0
+
+        i = 0
         for images, labels in train_loader:
+            # in case we use cuda to train on gpu
+            images = images.to(device)
+            labels = labels.to(device)
+
             optimizer.zero_grad()
 
-            log_ps = model(images)
-            loss = criterion(log_ps, labels)
+            preds = model(images)
+            loss = criterion(preds, labels)
             loss.backward()
             optimizer.step()
+            train_loss += loss.item()
 
-            running_loss += loss.item()
-            print(i)
+            # accuracy
+            _, preds_indices = torch.max(preds, dim=1)
+            train_correct += (preds_indices == labels).sum()
+
             i += 1
+            if i % 100 == 0:
+                print(
+                    f"Epoch: {e+1} / {epochs}"
+                    f" - progress: {i} / {batch_count}"
+                    f" - loss: {loss.data.mean()}"
+                )
 
-        print("loss:", running_loss/len(train_loader))
-        losses.append(running_loss)
+        for images, labels in validation_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            preds = model(images)
+            loss = criterion(preds, labels)
+
+            validation_loss += loss.item()
+
+            # accuracy
+            _, preds_indices = torch.max(preds, dim=1)
+            validation_correct += (preds_indices == labels).sum()
+
+        train_accuracy = float(train_correct / (len(train_loader) * batch_size))
+        validation_accuracy = float(validation_correct / (len(validation_loader) * batch_size))
+        print("Epoch:", e + 1)
+        print("Train loss:         ", train_loss / len(train_loader))
+        print("Validation loss:    ", validation_loss / len(validation_loader))
+        print("Train Accuracy:     ", train_accuracy)
+        print("Validation accuracy:", validation_accuracy)
+        print("Time:               ", time() - start_time)
+
+        start_time = time()
+        train_losses.append(train_loss / len(train_loader))
+        validation_losses.append(validation_loss / len(validation_loader))
 
     # plotting
-    plt.plot(list(range(epochs)), losses)
+    plt.plot(list(range(1, len(train_losses) + 1)), train_losses, label="Training loss")
+    print("Train losses:", train_losses)
+
+    plt.plot(list(range(1, len(validation_losses) + 1)), validation_losses, label="Validation loss")
+    print("Validation losses:", validation_losses)
+
     plt.xlabel("epoch")
     plt.ylabel("loss")
-    plt.savefig("reports/figures/training_run.png")
-    torch.save(model.state_dict(), "models/trained_model.pth")
+    plt.legend()
+
+    fig_path = Path("reports/figures/training_run.png")
+    plt.savefig(fig_path)
+    print(f"Saved training loss figure to {fig_path}")
+
+    model_path = Path("models/trained_model.pth")
+    torch.save(model.state_dict(), model_path)
+    print(f"Saved trained model to {model_path}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     train()
