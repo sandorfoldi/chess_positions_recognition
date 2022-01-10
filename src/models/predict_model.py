1cdf63f98006016ecce6974a398c982cb64c1bd7
Rasmus Spangsberg <rasmus.spangsberg@hotmail.com>
2022-01-11 00:44:53 +0100

    predict_model.py now correctly uses the model to predict the given image

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,20 +1,32 @@
 import argparse
 import sys
+from typing import List
 
+import numpy as np
 import torch
-from torchvision import transforms
 from model import ChessPiecePredictor
-from torch.utils.data import DataLoader
-import torchvision
+from PIL import Image
+from torchvision import transforms
+
+
+def crop(image: np.ndarray) -> List[np.ndarray]:
+    """This method was taken from src/data/make_dataset.py but modified
+    to grayscale instead of rgb images"""
+    parts = []
+    for r in range(0, image.shape[0], 50):
+        for c in range(0, image.shape[1], 50):
+            parts.append(image[r: r + 50, c: c + 50])
+    return parts
+
 
 def predict():
-    """Evaluates the given model on the given data set,
-    printing the accuracy."""
+    """Given a model and an image of an entire chess board (found in data/raw/ folder),
+    this method predicts what piece (if any) is on each square"""
     # parsing
-    print("Evaluating model...")
-    parser = argparse.ArgumentParser(description='Evaluation arguments')
-    parser.add_argument('load_model_from', default="")
-    parser.add_argument('load_data_from', default="")
+    print("Predicting squares...")
+    parser = argparse.ArgumentParser(description="Evaluation arguments")
+    parser.add_argument("load_model_from", default="")
+    parser.add_argument("image", default="")
     args = parser.parse_args(sys.argv[1:])
 
     # model loading
@@ -22,26 +34,33 @@ def predict():
     state_dict = torch.load(args.load_model_from)
     model.load_state_dict(state_dict)
     model.eval()
-    torch.set_grad_enabled(False)
 
-    # data loading
-    t = transforms.Compose([
-        transforms.ToTensor(),
-    ])
-    train_data = torchvision.datasets.ImageFolder(args.load_data_from, transform=t)
-    train_loader = DataLoader(train_data, batch_size=256,
-                              shuffle=False, num_workers=4)
+    # data loading and preparing
+    image = Image.open(args.image)
+    image = image.convert("L")  # convert to grayscale
 
-    # prediction
-    accuracy = 0
-    for images, labels in train_loader:
-        ps = torch.exp(model(images))
-        top_p, top_class = ps.topk(1, dim=1)
-        equals = top_class == labels.view(*top_class.shape)
-        accuracy += equals.type(torch.FloatTensor)
+    squares = crop(np.array(image))
+    squares = torch.tensor(
+        np.array(squares)
+    )  # convert to np.array() for speed. If not, we get a user warning
+    squares = transforms.ConvertImageDtype(torch.float)(squares)
 
-    accuracy = torch.mean(accuracy)
-    print(f"Accuracy: {accuracy.item() * 100}%")
+    # convert from (64, 50, 50), models expects the extra dimension
+    squares = squares.view(64, 1, 50, 50)
+
+    with torch.no_grad():
+        prediction = model(squares)
+
+    # show results
+    classes = ["_", "b", "k", "n", "p", "q", "r", "B", "K", "N", "P", "Q", "R"]
+
+    _, prediction_indices = torch.max(prediction, dim=1)
+    i = 0
+    for index in prediction_indices:
+        print(classes[index], end="")
+        i += 1
+        if i % 8 == 0:
+            print()
 
 
 if __name__ == "__main__":
