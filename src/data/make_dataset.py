dd0d67353813d2b19a81537bb6b1fda011216331
sandorfoldi <sandorfoldi98@gmail.com>
2022-01-12 23:45:21 +0100

    save processed data in tensors

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,10 +6,11 @@ import numpy as np
 from PIL import Image
 from tqdm import tqdm
 from numba import njit
-
+from tqdm import tqdm
 import torch
 from torchvision import transforms
 
+class_dict = {'e': 0, 'E': 0, 'r': 1, 'n': 2, 'b': 3, 'q': 4, 'k': 5, 'p': 6, 'R': 7, 'N': 8, 'B': 9, 'Q': 10, 'K': 11, 'P': 12}
 
 @njit
 def crop(image: np.ndarray) -> List[np.ndarray]:
@@ -35,71 +36,39 @@ def transform_label(filename: str) -> List[str]:
                 transformed_label.append(letter)
     return transformed_label
 
-'''
-def make_dataset(
-    input_dir: str = "data/raw/train", output_dir: str = "data/processed/train"
-) -> None:
-
-    dirs = [
-        "b_b",
-        "b_k",
-        "b_n",
-        "b_p",
-        "b_q",
-        "b_r",
-        "w_B",
-        "w_K",
-        "w_N",
-        "w_P",
-        "w_Q",
-        "w_R",
-        "w_E",
-    ]
-
-    if not os.path.exists(input_dir):
-        os.makedirs(input_dir)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    for item in dirs:
-        os.makedirs(f"{output_dir}/{item}", exist_ok=True)
-
-    files = os.listdir(input_dir)[:20000]
-
-    # for idx, file in enumerate(tqdm(files)):
-    for idx, file in enumerate(tqdm(files)):
-        folder_names = transform_label(file)
-        orig_image = Image.open(f"{input_dir}/{file}")
-        orig_image = np.array(orig_image)
-        cropped_images = crop(orig_image)
-        for i in range(len(folder_names)):
-            image = Image.fromarray(cropped_images[i])
-            if not folder_names[i].isupper():
-                image.save(f"{output_dir}/b_{folder_names[i]}/{idx}-{i}.jpeg")
-            else:
-                image.save(f"{output_dir}/w_{folder_names[i]}/{idx}-{i}.jpeg")
-    os.rename(f"{output_dir}/w_E", f"{output_dir}/E")
-'''
 
 def make_dataset(
     input_dir: str = "data/raw/train", output_dir: str = "data/processed/train_1"
 ) -> None:
-    list_raw_img = glob.glob(os.path.join(input_dir,'*.jpeg'))
-    img_per_slice = 100000
-    img_size_processed = (50, 50, 3)
-    slice_num = int(len(list_raw_img) * 64 / img_per_slice) # there might be some problems later with rounding integers here
-    # print(slice_num)
-    for slice_ind in range(slice_num):
-        t = torch.Tensor(img_per_slice,  img_size_processed[2], img_size_processed[0],img_size_processed[1],)
-        print(t.shape)
-        for i in range(img_per_slice):
-            print(t[i].shape)
-            print(transforms.ToTensor()(Image.open(list_raw_img[slice_ind*img_per_slice+i])).shape)
-            break
-            t[i] = transforms.ToTensor()(Image.open(list_raw_img[slice_ind*img_per_slice+i]))
-        t.save(output_dir+'imgs_train_'+str(slice_ind))
-        break
-    
 
+    list_img = glob.glob(os.path.join(input_dir,'*.jpeg'))[:1024]
+
+    squares_per_slice = 2**14
+    images_per_slice = int(squares_per_slice / 64)
+    
+    square_size = (50, 50, 3)
+
+    num_images = len(list_img)
+    num_squares = num_images * 64 
+    num_slices = int(num_images / images_per_slice) 
+
+    list_labels = []
+    for image_path in list_img:
+        list_labels.extend(transform_label(image_path.split('/')[-1]))
+    list_labels = list(map(lambda l: class_dict[l], list_labels))
+    tensor_labels = torch.Tensor(list_labels)
+    torch.save(tensor_labels, 'data/processed/train_1/labels_train.pt')
+
+    for slice_ind in range(num_slices):
+        t = torch.Tensor(squares_per_slice, square_size[0],square_size[1], square_size[2], )
+        for image_ind in range(images_per_slice):
+            img = np.array(Image.open(list_img[image_ind]))
+            squares = crop(img)
+            squares = np.stack(squares, axis=0)
+            t[image_ind*64:(image_ind+1)*64] = torch.from_numpy(squares)
+
+        torch.save(t, output_dir+'/imgs_train_'+str(slice_ind)+'.pt')
+        
+    
 if __name__ == "__main__":
     make_dataset()
